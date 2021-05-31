c26e938adde585fddf8c05a20c3faaccfce8f5a1
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-31 09:39:34 +0200

    Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Operators
 /*
-Arithmetic Operators
+// Arithmetic Operators
 
 x + y;      Addition
 x - y;      Subtraction
@@ -9,19 +9,19 @@ x / y;      Division
 x % y;      Modulus (Remainder after division)
 x ** y;     Exponent (To the power of)
 
-Increment Operator (++)
+// Increment Operator (++)
 console.log(++x);       console shows 11
 
 console.log(x++); // console shows 10
 console.log(x); // console shows 11
 
-Decrement Operator (--)
+// Decrement Operator (--)
 console.log(--x); // console shows 9
 
 console.log(x--); // console shows 10
 console.log(x); // console shows 9
 
-Assignment Operators
+// Assignment Operators
 x++;
 x = x + 1;
 These statements mean the same thing
@@ -30,4 +30,37 @@ x += 5;
 
 x = x * 3;
 x *= 3;
+
+// Relational Operators
+let x = 1;
+
+Greater Than (>):
+console.log(x > 0);     retursn true
+
+Greater Than or Equal To (>=):
+console.log(x >= 1);        returns true
+
+Less Than (<):
+console.log(x < 1);     returns false
+
+Less Than or Equal To (<=):
+console.log(x <= 1);        returns true
+
+// Equality Operators
+let x = 1;
+
+// Is x equal to 1?
+console.log(x === 1);       returns true
+
+// Is x not equal to 1?
+console.log(x !== 1);       returns false. X is equal to 1
+
+// Strict Equality
+console.log(1 === 1);		// true
+console.log(‘1’ === 1);	// false
+
+// Loose Equality
+console.log(1 == 1);		// true
+console.log(‘1’ == 1);	// true
+console.log(true == 1);	// true
 */
