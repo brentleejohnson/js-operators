7afd46520924bd85564eadd1ab6439d34cf75f03
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-31 09:31:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Operators
+/*
+Arithmetic Operators
+
+x + y;      Addition
+x - y;      Subtraction
+x * y;      Multiplication
+x / y;      Division
+x % y;      Modulus (Remainder after division)
+x ** y;     Exponent (To the power of)
+
+Increment Operator (++)
+console.log(++x);       console shows 11
+
+console.log(x++); // console shows 10
+console.log(x); // console shows 11
+
+Decrement Operator (--)
+console.log(--x); // console shows 9
+
+console.log(x--); // console shows 10
+console.log(x); // console shows 9
+
+Assignment Operators
+x++;
+x = x + 1;
+These statements mean the same thing
+
+x += 5;
+
+x = x * 3;
+x *= 3;
+*/
